e36d613e5180311b7a3cacec679455d3bdf672c9
Rodrigo Reis <59849294+rodrigo1835@users.noreply.github.com>
2023-04-16 14:51:06 -0300

    Add files via upload

---
+++ Lista_1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,a,valor;
+    cin >> n >> a;
+
+    if(n >= 500)
+        valor = n % 500;
+
+    else{
+        valor = n;
+    }
+
+    if(a - valor >= 0 && valor != 0)
+        cout << "Sim" << "\n";
+    else if (valor == 0)
+        cout << "Sim" << "\n";
+    else
+        cout << "Nao" << "\n";
+
+
+}
